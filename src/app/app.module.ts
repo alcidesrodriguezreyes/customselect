143393d6ff537bc2dec5da9ed4b8b1a1c78efda8
Alcides Rodríguez Reyes <alcides.rodriguez@globant.com>
2016-10-05 15:14:13 -0500

    multiselect initial files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { MultiselectComponent } from './multiselect/multiselect.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MultiselectComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/multiselect/multiselect.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { MultiselectComponent } from './multiselect.component';
+
+describe('Component: Multiselect', () => {
+  it('should create an instance', () => {
+    let component = new MultiselectComponent();
+    expect(component).toBeTruthy();
+  });
+});
