a1e1c10607be979efa612ecd351d89b9c0b841fe
Alcides Rodríguez Reyes <alcides.rodriguez@globant.com>
2016-10-06 10:27:31 -0500

    multiselect removed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,10 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { MultiselectComponent } from './multiselect/multiselect.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MultiselectComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
